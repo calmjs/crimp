eb1894dc472e99fce27f3c27690c85bac0f7b664
Tommy Yu <tommy.yu@auckland.ac.nz>
2018-08-11 18:17:43 +1200

    Bumping minimum calmjs.parse version.

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'calmjs.parse',
+        'calmjs.parse>=1.1.1',
     ],
     entry_points={
         'console_scripts': [
